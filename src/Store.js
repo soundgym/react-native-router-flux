30e4e2b3ace65a72ef886e9808d70833065c5898
StudyTae <dea972@naver.com>
2022-11-27 19:48:38 +0900

    modify data

--- src/Store.js
+++ src/Store.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { Animated, Easing, Image } from 'react-native';
 import { NavigationActions, StackActions } from 'react-navigation';
 import { createDrawerNavigator, DrawerActions } from 'react-navigation-drawer';
-import createStackNavigatorFlux from 'react-navigation-stack/src/navigators/createStackNavigator';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation-tabs';
 import _drawerImage from '../images/menu_burger.png';
 import * as ActionConst from './ActionConst';
@@ -857,7 +857,7 @@ export default class NavigationStore {
         navigationOptions: createNavigationOptions(commonProps),
       });
     }
-    return createStackNavigatorFlux(res, {
+    return createStackNavigator(res, {
       mode,
       initialRouteParams,
       initialRouteName,
